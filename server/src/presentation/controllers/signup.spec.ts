16257e041f3579874081d13c866362b0a6ac8ed0
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-02-28 20:16:27 -0300

    test: ensure SignUpController return 400 if no username is provided

---
+++ server/src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,30 @@
+import { SignUpController } from './signup'
+import { MissingParamsError } from '../errors/missing-params-error'
+
+const makeSut = (): SignUpController => {
+    return new SignUpController()
+}
+
+describe('SignUp Controller', () => {
+
+    test('Should return 400 if no username is provided', () => {
+
+        const sut = makeSut()
+
+        const httpRequest = {
+            body: {
+                email: '_any@email',
+                birth_date: '2021-02-28',
+                name: '_any_name',
+                password: '_any_password',
+                passwordConfirm: '_any_passwordConfirm'
+            }
+        }
+
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new MissingParamsError('username'))
+
+    })
+
+})
