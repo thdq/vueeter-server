9e390c88382c1b6856606b411bebc849c4210640
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-02-28 20:32:10 -0300

    test: ensure SignUpController return 400 if no email is provided

--- server/src/presentation/controllers/signup.spec.ts
+++ server/src/presentation/controllers/signup.spec.ts
@@ -27,4 +27,24 @@ describe('SignUp Controller', () => {
 
     })
 
+    test('Should return 400 if no email is provided', () => {
+
+        const sut = makeSut()
+
+        const httpRequest = {
+            body: {
+                username: '_any_username',
+                birth_date: '2021-02-28',
+                name: '_any_name',
+                password: '_any_password',
+                passwordConfirm: '_any_passwordConfirm'
+            }
+        }
+
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new MissingParamsError('email'))
+
+    })
+
 })
