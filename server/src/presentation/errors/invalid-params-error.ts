984f63c0457b3966f8b45f1e15e2843ade4afc81
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-02-28 23:48:47 -0300

    feat: custom error from invalid params in request

---
+++ server/src/presentation/errors/invalid-params-error.ts
@@ -0,0 +1,8 @@
+export class InvalidParamsError extends Error {
+
+    constructor (paramName: string) {
+        super(`Invalid param: ${paramName}`)
+        this.name = 'InvalidParamsError'
+    }
+
+}
