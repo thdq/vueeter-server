a01534f914ae1baa36ead0a26dd23f715a7b86a4
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-03-04 01:32:17 -0300

    feature: server success helper

--- server/src/presentation/helpers/http.ts
+++ server/src/presentation/helpers/http.ts
@@ -10,3 +10,8 @@ export const serverError = (): HttpResponse => ({
     statusCode: 500,
     body: new ServerError()
 })
+
+export const serverSuccess = (body: any): HttpResponse => ({
+    statusCode: 200,
+    body: body
+})
