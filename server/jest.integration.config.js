8b905cc9d85bd9214a76f421dd452f0c717c9f4f
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-03-14 22:25:18 -0300

    refactor: add jest config from manage databases with custom jest environment

--- server/jest.integration.config.js
+++ server/jest.integration.config.js
@@ -1,7 +1,6 @@
 let config = require('./jest.config')
-const path = require('path')
 
 config.testMatch = ['**/*.test.ts']
-config.testEnvironment = path.join(__dirname, 'prisma', 'prisma-test-environment.js')
+
 
 module.exports = config
--- server/jest.unit.config.js
+++ server/jest.unit.config.js
@@ -1,5 +1,8 @@
 const config = require('./jest.config')
 
 config.testMatch = ['**/*.spec.ts']
+config.testPathIgnorePatterns = [
+    "<rootDir>/src/infra/database/",
+    "<rootDir>/node_modules/"]
 
 module.exports = config
