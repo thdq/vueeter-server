8d6c8d8939475f43fe4bf7e2beb963a5913c653e
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-03-19 00:06:11 -0300

    feat: ensure Login controller calls Authenticator with correct values

--- server/src/presentation/controllers/login/login.ts
+++ server/src/presentation/controllers/login/login.ts
@@ -1,19 +1,36 @@
+import { Authentication } from '../../../domain/usecases/authentication'
 import { MissingParamsError } from '../../../presentation/errors'
-import { badRequest } from '../../../presentation/helpers/http'
+import { badRequest, serverError } from '../../../presentation/helpers/http'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 
 export class LoginController implements Controller {
+    private readonly authentication: Authentication
+    
+    constructor (authentication: Authentication) {
+        this.authentication = authentication
+    }
+    
     async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+        
+        try {
+            
+            const { username, password } = httpRequest.body
+            
+            const requiredFields = ['username', 'password']
 
-        const requiredFields = ['username', 'password']
-
-        for (const field of requiredFields) {
-
-            if (!httpRequest.body[field]) return badRequest(new MissingParamsError(field))
-
+            for (const field of requiredFields) {
+    
+                if (!httpRequest.body[field]) return badRequest(new MissingParamsError(field))
+    
+            }
+            
+            await this.authentication.auth(username, password)
+            
+            return new Promise(resolve => resolve(null))            
+            
+        } catch (error) {
+            return serverError()
         }
-        
-        return new Promise(resolve => resolve(null))
-        
+
     }
 }
