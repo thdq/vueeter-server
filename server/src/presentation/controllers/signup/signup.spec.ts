17f21f8a97763992a944d31c1f2292f37157873c
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-03-20 00:51:17 -0300

    test: ensure SignUpController calls Validation with correct value

--- server/src/presentation/controllers/signup/signup.spec.ts
+++ server/src/presentation/controllers/signup/signup.spec.ts
@@ -1,11 +1,12 @@
 import { SignUpController } from './signup'
 import { MissingParamsError, ServerError, InvalidParamsError } from '../../errors'
-import { EmailValidator, UserModel, AddUser, AddUserModel } from './signup.protocols'
+import { EmailValidator, UserModel, AddUser, AddUserModel, Validation } from './signup.protocols'
 
 interface SutTypes {
     sut: SignUpController
     emailValidatorStub: EmailValidator
     addUserStub: AddUser
+    validationStub: Validation
 }
 
 const makeEmailValidator = (): EmailValidator => {
@@ -41,17 +42,28 @@ const makeAddUser = (): AddUser => {
     return new AddUserStub()
 }
 
+const makeValidation = (): Validation => {
+    class ValidationStub implements Validation {
+        validate (input: any): Error {
+            return null
+        }
+    }
+    return new ValidationStub()
+}
+
 const makeSut = (): SutTypes => {
 
     const emailValidatorStub = makeEmailValidator()
     const addUserStub = makeAddUser()
+    const validationStub = makeValidation()
 
-    const sut = new SignUpController(emailValidatorStub, addUserStub)
+    const sut = new SignUpController(emailValidatorStub, addUserStub, validationStub)
 
     return {
         sut,
         emailValidatorStub,
-        addUserStub
+        addUserStub,
+        validationStub
     }
 }
 
@@ -312,5 +324,27 @@ describe('SignUp Controller', () => {
         const httpResponse = await sut.handle(httpRequest)
         expect(httpResponse.statusCode).toBe(200)
     })
+    
+    test('Should call Validation with correct value', async () => {
+        
+        const { sut, validationStub } = makeSut()
+        
+        const validateSpy = jest.spyOn(validationStub, 'validate')
+        
+        const httpRequest = {
+            body: {
+                username: '_any_username',
+                birth_date: '2021-02-28',
+                email: '_any_mail@email',
+                name: '_any_name',
+                password: '_any_password',
+                passwordConfirm: '_any_password'
+            }
+        }
+        
+        await sut.handle(httpRequest)
+        expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+        
+    })    
 
 })
