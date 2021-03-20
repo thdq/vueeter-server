456ca9e5a66d9ae9e5005b0f7cb86648e93887b3
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-03-20 19:41:41 -0300

    test: ensure MaxLengthField validator return a InvalidParamsError if validation fails

---
+++ server/src/presentation/helpers/validators/max-length-fields-validation.spec.ts
@@ -0,0 +1,24 @@
+import { InvalidParamsError } from "../../errors"
+import { MaxLengthFieldValidation } from "./"
+
+const makeSut = (): MaxLengthFieldValidation => {
+    
+    return new MaxLengthFieldValidation('username')
+    
+}
+
+describe('MaxLengthField Validation', () => {
+    
+    test('Should return a InvalidParamsError if validation fails', () => {
+        
+        const sut = makeSut()
+        
+        const error = sut.validate({
+            username: '_invalid_username_exceeds_50_characters_invalid_username_exceeds_50_characters'
+        })
+        
+        expect(error).toEqual(new InvalidParamsError('username'))
+        
+    })  
+    
+})
