4fd76c3496ccfeddaff412775262c1094c38a5c0
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-03-20 20:45:21 -0300

    feat: ensure PasswordRulesFieldValidation return a InvalidParamsError if password not contains number

---
+++ server/src/presentation/helpers/validators/password-rules-field-validation.ts
@@ -0,0 +1,20 @@
+import { InvalidParamsError } from "../../../presentation/errors"
+import { Validation } from "../../../presentation/controllers/signup/signup.protocols"
+
+export class PasswordRulesFieldValidation implements Validation {
+    private readonly fieldName: string
+    
+    constructor (fieldName: string) {
+        this.fieldName = fieldName
+    }
+    
+    validate (input: any): Error {
+        
+        const HAVENUMBER = /\d/.test(input[this.fieldName])
+        const HAVEUPPERCASE = /[A-Z]/.test(input[this.fieldName])
+        const HAVEMOREOREQUALTHAN8CHARACTERS = input[this.fieldName].length >= 8
+        
+        if (!HAVENUMBER || !HAVEUPPERCASE || !HAVEMOREOREQUALTHAN8CHARACTERS) return new InvalidParamsError(this.fieldName)
+    }
+    
+}
