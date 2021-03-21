9fc29f75b93e09f199ace67189c6d01e5503853f
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-03-20 21:53:35 -0300

    chore: remove constraint to table user_favourited and user_revueeted

---
+++ server/prisma/migrations/20210321004900_/migration.sql
@@ -0,0 +1,5 @@
+-- DropForeignKey
+ALTER TABLE "user_favourited" DROP CONSTRAINT "user_favourited_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "user_revueeted" DROP CONSTRAINT "user_revueeted_userId_fkey";
