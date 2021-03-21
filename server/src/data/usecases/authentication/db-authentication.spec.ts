95a1764a831a82c26e7f28d869753f9023059e53
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-03-21 15:39:50 -0300

    test: ensure DbAuthentication calls LoadUserByUsernameRepository with correct username

---
+++ server/src/data/usecases/authentication/db-authentication.spec.ts
@@ -0,0 +1,59 @@
+import { UserModel } from "../../../domain/models/user"
+import { LoadUserByUsernameRepository } from "../../../data/protocols/repository/load-user-by-username"
+import { DbAuthentication } from './db-authentication'
+
+const makeLoadUserByUsernameRepository = (): LoadUserByUsernameRepository => {
+    
+    class LoadUserByUsernameRepositoryStub implements LoadUserByUsernameRepository {
+        async load (username: string): Promise<UserModel> {
+            
+            const user: UserModel = {
+                id: '_valid_id',
+                username: 'thdq',
+                birth_date: new Date('2021-03-21'),
+                email: '_valid@email',
+                name: '_any_name',
+                password: '_any_password'
+            }
+            
+            return new Promise(resolve => resolve(user))
+        }
+    }
+    
+    return new LoadUserByUsernameRepositoryStub()
+}
+
+interface SutTypes {
+    sut: DbAuthentication
+    loadUserByUsernameRepositoryStub: LoadUserByUsernameRepository
+}
+
+const makeSut = (): SutTypes => {
+    
+    const loadUserByUsernameRepositoryStub = makeLoadUserByUsernameRepository()
+    const sut = new DbAuthentication(loadUserByUsernameRepositoryStub)
+    
+    return {
+        sut,
+        loadUserByUsernameRepositoryStub
+    }
+}
+
+describe('DbAuthentication UseCase', () => {
+    
+    test('Should call LoadUserByUsernameRepository with correct email', async () => {
+        
+        const { sut, loadUserByUsernameRepositoryStub } = makeSut()
+        
+        const loadSpy = jest.spyOn(loadUserByUsernameRepositoryStub, 'load')
+        
+        await sut.auth({
+            username: 'thdq',
+            password: '_any_password'
+        })
+        
+        expect(loadSpy).toHaveBeenCalledWith('thdq')
+        
+    })
+    
+})
