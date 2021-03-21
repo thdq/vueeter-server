92f46a46f3a4276be4e70e5a0a8d85a4b8f9e812
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-03-21 17:48:22 -0300

    feat: ensure DbAuthentication calls TokenGenerator with correct id

--- server/src/data/usecases/authentication/db-authentication.ts
+++ server/src/data/usecases/authentication/db-authentication.ts
@@ -1,21 +1,27 @@
 import { AuthenticationModel } from "../../../domain/usecases"
 import { LoadUserByUsernameRepository } from "../../../data/protocols/repository/load-user-by-username"
 import { HashCompare } from "../../../data/protocols/cripotagraphy/hash-compare"
+import { TokenGenerator } from "../../../data/protocols/cripotagraphy/token-generator"
 
 export class DbAuthentication {
     private readonly loadUserByUsernameRepository: LoadUserByUsernameRepository
     private readonly hashCompare: HashCompare
+    private readonly tokenGenerator: TokenGenerator
     
-    constructor (loadUserByUsernameRepository: LoadUserByUsernameRepository, hashCompare: HashCompare) {
+    constructor (loadUserByUsernameRepository: LoadUserByUsernameRepository, hashCompare: HashCompare, tokenGenerator: TokenGenerator) {
         this.loadUserByUsernameRepository = loadUserByUsernameRepository
         this.hashCompare = hashCompare
+        this.tokenGenerator = tokenGenerator
     }
     
     async auth (authentication: AuthenticationModel): Promise<string> {
         
         const user = await this.loadUserByUsernameRepository.load(authentication.username)
         
-        if (user) await this.hashCompare.compare(authentication.password, user.password)
+        if (user) {
+            await this.hashCompare.compare(authentication.password, user.password)
+            await this.tokenGenerator.generate(user.id)
+        }
         
         return null
     }
