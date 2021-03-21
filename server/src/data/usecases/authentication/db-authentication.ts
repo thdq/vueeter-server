a9ab5afb9f1b983367dcad823236913376a2893c
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-03-21 17:30:00 -0300

    feat: ensure DbAuthentication calls HashCompare with correct values

--- server/src/data/usecases/authentication/db-authentication.ts
+++ server/src/data/usecases/authentication/db-authentication.ts
@@ -1,15 +1,21 @@
 import { AuthenticationModel } from "../../../domain/usecases"
 import { LoadUserByUsernameRepository } from "../../../data/protocols/repository/load-user-by-username"
+import { HashCompare } from "../../../data/protocols/cripotagraphy/hash-compare"
 
 export class DbAuthentication {
     private readonly loadUserByUsernameRepository: LoadUserByUsernameRepository
+    private readonly hashCompare: HashCompare
     
-    constructor (loadUserByUsernameRepository: LoadUserByUsernameRepository) {
+    constructor (loadUserByUsernameRepository: LoadUserByUsernameRepository, hashCompare: HashCompare) {
         this.loadUserByUsernameRepository = loadUserByUsernameRepository
+        this.hashCompare = hashCompare
     }
     
     async auth (authentication: AuthenticationModel): Promise<string> {
-        await this.loadUserByUsernameRepository.load(authentication.username)
+        
+        const user = await this.loadUserByUsernameRepository.load(authentication.username)
+        
+        if (user) await this.hashCompare.compare(authentication.password, user.password)
         
         return null
     }
