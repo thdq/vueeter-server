06c64ceace71bec7bf0c0be22a9ea18ad8c345f6
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-03-22 20:48:23 -0300

    feat: ensure UserRepository returns a user if loadByUsername succeeds

--- server/src/infra/database/postgresql/user-repository/user.ts
+++ server/src/infra/database/postgresql/user-repository/user.ts
@@ -1,9 +1,10 @@
 import { AddUserRepository } from '@/data/protocols/repository/add-user-repository'
+import { LoadUserByUsernameRepository } from '@/data/protocols/repository/load-user-by-username'
 import { UserModel } from '@/domain/models/user'
 import { AddUserModel } from '@/domain/usecases/add-user'
 import { prisma, id } from '../helpers'
 
-export class UserPostgreSQLRepository implements AddUserRepository {
+export class UserPostgreSQLRepository implements AddUserRepository, LoadUserByUsernameRepository {
     
     async add (user: AddUserModel): Promise<UserModel> {
         
@@ -12,6 +13,18 @@ export class UserPostgreSQLRepository implements AddUserRepository {
         })
         
         return new Promise(resolve => resolve(userResponse))
+    }
+    
+    async loadByUsername (username: string): Promise<UserModel> {
+        
+        const userResponse = await prisma.user.findUnique({
+            where: {
+                username: username
+            }
+        })
+        
+        return userResponse
+        
     }
     
 }
