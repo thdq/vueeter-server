a22832c6fc763d4ea936891aa11f0f79f51fc800
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-03-22 22:08:19 -0300

    refactor: group by folder repository protocols

--- server/src/data/usecases/add-user/db-add-user.protocol.ts
+++ server/src/data/usecases/add-user/db-add-user.protocol.ts
@@ -1,4 +1,4 @@
 export * from '@/domain/usecases/add-user'
 export * from '@/domain/models/user'
 export * from '@/data/protocols/cripotagraphy/hasher'
-export * from '@/data/protocols/repository/add-user-repository'
+export * from '@/data/protocols/repository/user/add-user-repository'
