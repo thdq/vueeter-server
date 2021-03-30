eade846f30ca2dc1cca13c67390e494b6666d48c
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-03-29 22:53:32 -0300

    feat: ensure DbAddUser calls LoadUserByUsernameRepository with correct username

--- server/src/data/usecases/add-user/db-add-user.ts
+++ server/src/data/usecases/add-user/db-add-user.ts
@@ -1,15 +1,19 @@
-import { Hasher, UserModel, AddUserModel, AddUser, AddUserRepository } from "./db-add-user.protocol"
+import { Hasher, UserModel, AddUserModel, AddUser, AddUserRepository, LoadUserByUsernameRepository } from "./db-add-user.protocol"
 
 export class DbAddUser implements AddUser {
     private readonly hasher: Hasher
     private readonly addUserRepository: AddUserRepository
+    private readonly loadUserByUsernameRepository: LoadUserByUsernameRepository
 
-    constructor (hasher: Hasher, addUserRepository: AddUserRepository) {
+    constructor (hasher: Hasher, addUserRepository: AddUserRepository, loadUserByUsernameRepository: LoadUserByUsernameRepository) {
         this.hasher = hasher
         this.addUserRepository = addUserRepository
+        this.loadUserByUsernameRepository = loadUserByUsernameRepository
     }
     
     async add (user: AddUserModel): Promise<UserModel> {
+        
+        await this.loadUserByUsernameRepository.loadByUsername(user.username)
         
         const hashedPass = await this.hasher.hash(user.password)
         
