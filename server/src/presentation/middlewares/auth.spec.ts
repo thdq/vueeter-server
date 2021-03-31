7477bca4988b060ebb59d003c28755d5e7540e0f
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-03-30 22:34:54 -0300

    test: ensure AuthMiddleware calls LoadUserByToken with correct access token

--- server/src/presentation/middlewares/auth.spec.ts
+++ server/src/presentation/middlewares/auth.spec.ts
@@ -1,23 +1,44 @@
-import { AccessDeniedError } from '../errors/access-denied-error'
-import { forbidden } from '../helpers/http'
-import { HttpRequest } from '../protocols'
+import { loadUserByToken, HttpRequest, forbidden, AccessDeniedError, UserModel } from './auth.protocol'
 import { AuthMiddleware } from './auth'
 
 interface SutTypes {
     sut: AuthMiddleware
+    loadUserByTokenStub: loadUserByToken
+}
+
+const makeLoadUserByToken = (): loadUserByToken => {
+
+    class LoadUserByTokenStub implements loadUserByToken {
+        async load (httpRequest: HttpRequest): Promise<UserModel> {
+            return new Promise(resolve => resolve({
+                id: '_any_id',
+                birth_date: new Date('2021-03-30'),
+                name: '_any_name',
+                email: '_any@email.com',
+                password: '_any_password',
+                username: '_any_usernamme'
+            }))
+        }
+    }
+    
+    return new LoadUserByTokenStub()
+    
 }
 
 const makeSut = (): SutTypes => {
     
-    const sut = new AuthMiddleware()
+    const loadUserByTokenStub = makeLoadUserByToken()
+    const sut = new AuthMiddleware(loadUserByTokenStub)
     
     return {
-        sut
+        sut,
+        loadUserByTokenStub
     }
     
 }
 
 describe('Auth middleware', () => {
+    
     test('Should return 403 if no x-access-token is provided in header', async () => {
         
         const { sut } = makeSut()
@@ -30,4 +51,26 @@ describe('Auth middleware', () => {
         
         expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
     })
+    
+    test('Should call LoadUserByToken with correct access token', async () => {
+        
+        const { sut, loadUserByTokenStub } = makeSut()
+        
+        const httpRequest: HttpRequest = {
+            headers: {
+                "x-access-token": "_valid_token"
+            }
+        }
+        
+        const loadSpy = jest.spyOn(loadUserByTokenStub, 'load')
+        
+        await sut.handle(httpRequest)
+        
+        expect(loadSpy).toHaveBeenCalledWith({
+            headers: {
+                "x-access-token": "_valid_token"
+            }            
+        })
+    })
+        
 })
