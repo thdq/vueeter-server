a0591144b1e3df5eedcf901666fd64cd90798917
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-03-30 23:03:29 -0300

    feat: ensure AuthMiddleware return 200 if LoadUserByToken returns a user

--- server/src/presentation/middlewares/auth.ts
+++ server/src/presentation/middlewares/auth.ts
@@ -1,6 +1,6 @@
 import { loadUserByToken } from '../../domain/usecases/load-user-by-username'
 import { AccessDeniedError } from '../errors/access-denied-error'
-import { forbidden } from '../helpers/http'
+import { forbidden, serverSuccess } from '../helpers/http'
 import { HttpRequest, HttpResponse, Middleware } from '../protocols'
 
 export class AuthMiddleware implements Middleware {
@@ -14,7 +14,17 @@ export class AuthMiddleware implements Middleware {
         
         const accessToken = httpRequest.headers?.['x-access-token']
         
-        if (accessToken) await this.loadUserByToken.load(accessToken)
+        if (accessToken) {
+            
+            const user = await this.loadUserByToken.load(accessToken)            
+            
+            if (user) {
+                return serverSuccess({
+                    userId: user.id
+                })
+            }
+            
+        }
         
         return forbidden(new AccessDeniedError())
     }
