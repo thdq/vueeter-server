00f5eb20fb43f50eb47175daada40cb11d2847f0
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-04-02 22:36:24 -0300

    feat: factory to auth middleware

---
+++ server/src/main/factories/middlewares/auth-middleware.ts
@@ -0,0 +1,9 @@
+import { AuthMiddleware } from "@/presentation/middlewares/auth"
+import { Middleware } from "@/presentation/protocols"
+import { makeDbLoadUserByToken } from "../usecases/load-user-by-token/db-load-user-by-token"
+
+export const makeAuthMiddleware = (): Middleware => {
+    
+    return new AuthMiddleware(makeDbLoadUserByToken())
+    
+}
---
+++ server/src/main/factories/usecases/load-user-by-token/db-load-user-by-token.ts
@@ -0,0 +1,15 @@
+import { UserPostgreSQLRepository } from '../../../../infra/database/postgresql/user-repository/user'
+import { loadUserByToken } from '@/domain/usecases/load-user-by-token'
+import { DbLoadUserByToken } from '@/data/usecases/load-user-by-token/db-load-user-by-token'
+import { JwtAdapter } from '@/infra/criptography/jwt-adapter/jwt-adapter'
+import env from '../../../config/env'
+
+export const makeDbLoadUserByToken = (): loadUserByToken => {
+    
+    const userRepo = new UserPostgreSQLRepository()
+    
+    const jwtAdapter = new JwtAdapter(env.JWTSECRET)
+    
+    return new DbLoadUserByToken(jwtAdapter, userRepo)
+
+}
