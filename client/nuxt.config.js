c2be2b295dee5dee9ef27c7dba7d92c4358fe709
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-04-03 23:34:55 -0300

    chore: add middleware to nuxt config

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -1,5 +1,4 @@
 export default {
-    // Global page headers: https://go.nuxtjs.dev/config-head
     srcDir: 'src/',
     head: {
         title: 'vueeter',
@@ -12,47 +11,30 @@ export default {
             { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
         ]
     },
-
-    // Global CSS: https://go.nuxtjs.dev/config-css
     css: [
         'vuesax/dist/vuesax.css'
     ],
-
-    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
     plugins: [
         '@/plugins/vuesax'
     ],
-
-    // Auto import components: https://go.nuxtjs.dev/config-components
     components: true,
-
-    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
     buildModules: [
-    // https://go.nuxtjs.dev/typescript
         '@nuxt/typescript-build',
-        // https://go.nuxtjs.dev/stylelint
         '@nuxtjs/stylelint-module'
     ],
-
-    // Modules: https://go.nuxtjs.dev/config-modules
     modules: [
-    // https://go.nuxtjs.dev/axios
         '@nuxtjs/axios',
-        // https://go.nuxtjs.dev/pwa
         '@nuxtjs/pwa'
     ],
-
-    // Axios module configuration: https://go.nuxtjs.dev/config-axios
     axios: {},
-
-    // PWA module configuration: https://go.nuxtjs.dev/pwa
     pwa: {
         manifest: {
             lang: 'en'
         }
     },
-
-    // Build Configuration: https://go.nuxtjs.dev/config-build
     build: {
+    },
+    router: {
+        middleware: ['auth']
     }
 }
