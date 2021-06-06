e7fd2a0242480a561f4e824d7d6131d8b389cf7a
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-06-06 18:05:43 -0300

    refactor: simplify code and fix paths

--- src/main/adapters/express/express-middleware-adapter.ts
+++ src/main/adapters/express/express-middleware-adapter.ts
@@ -5,12 +5,7 @@ export const adaptMiddleware = (middleware: Middleware) => {
     
     return async (req: Request, res: Response, next: NextFunction) => {
         
-        const request = {
-            accessToken: req.headers?.['x-access-token'],
-            ...(req.headers || {})
-        }
-        
-        const httpResponse = await middleware.handle(request)
+        const httpResponse = await middleware.handle(req)
         
         if (httpResponse.statusCode === 200) {
             
--- src/main/factories/middlewares/auth-middleware.ts
+++ src/main/factories/middlewares/auth-middleware.ts
@@ -1,5 +1,5 @@
-import { AuthMiddleware } from "@/presentation/middlewares/auth"
-import { Middleware } from "@/presentation/protocols"
+import { AuthMiddleware } from "../../../presentation/middlewares/auth"
+import { Middleware } from "../../../presentation/protocols"
 import { makeDbLoadUserByToken } from "../usecases/load-user-by-token/db-load-user-by-token"
 
 export const makeAuthMiddleware = (): Middleware => {
