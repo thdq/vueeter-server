969dfa0f8ecb16091dbdd0a6c293814ae2016598
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-06-06 18:40:31 -0300

    test: ensure NewVueetController return 500 if valiadtion throws

--- src/presentation/controllers/new-vueet/new-vueet.spec.ts
+++ src/presentation/controllers/new-vueet/new-vueet.spec.ts
@@ -1,11 +1,25 @@
 import { NewVueet, AddVueetModel } from '@/domain/usecases/new-vueet'
 import { ServerError } from '@/presentation/errors'
+import { Validation } from '@/presentation/protocols'
 import faker from 'faker'
 import { NewVueetController } from './new-vueet'
 
 interface SutTypes {
     sut: NewVueetController
     newVueetStub: NewVueet
+    validationStub: Validation
+}
+
+const makeValidation = (): Validation => {
+    class ValidationStub implements Validation {
+        
+        validate (input: any): Error {
+            return null
+        }
+    }
+    
+    return new ValidationStub()
+    
 }
 
 const makeNewVueet = (): NewVueet => {
@@ -24,11 +38,13 @@ const makeNewVueet = (): NewVueet => {
 const makeSut = (): SutTypes => {
     
     const newVueetStub = makeNewVueet()
-    const sut = new NewVueetController(newVueetStub)
+    const validationStub = makeValidation()
+    const sut = new NewVueetController(newVueetStub, validationStub)
     
     return {
         sut,
-        newVueetStub
+        newVueetStub,
+        validationStub
     }
     
 }
@@ -58,5 +74,29 @@ describe('NewVueet Controller', () => {
         expect(httpResponse.body).toEqual(new ServerError())
 
     })
+    
+    test('Should return 500 if valiadtion throws', async () => {
+
+        const { sut, validationStub } = makeSut()
+        
+        jest.spyOn(validationStub, 'validate').mockImplementationOnce(() => {
+            throw new Error()
+        })
+                
+        const httpRequest = {
+            body: {
+                text: faker.random.word(),
+                source: faker.internet.userAgent(),
+                in_reply_to_vueet_id: null,
+                in_reply_to_user_id: null,
+                user_id: faker.datatype.uuid()
+            }
+        }
+        
+        const httpResponse = await sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(500)
+        expect(httpResponse.body).toEqual(new ServerError())
+
+    })    
 
 })
