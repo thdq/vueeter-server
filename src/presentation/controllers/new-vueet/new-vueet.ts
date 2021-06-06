ea14ce1e1ee181fb90139d6765016b70bc61be6d
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-06-06 18:38:24 -0300

    feat: ensure NewVueetController return 500 if valiadtion throws

--- src/presentation/controllers/new-vueet/new-vueet.ts
+++ src/presentation/controllers/new-vueet/new-vueet.ts
@@ -1,12 +1,14 @@
 import { AddVueetModel, NewVueet } from "@/domain/usecases/new-vueet"
-import { created, serverError } from "@/presentation/helpers/http"
-import { Controller, HttpRequest, HttpResponse } from "@/presentation/protocols"
+import { badRequest, created, serverError } from "@/presentation/helpers/http"
+import { Controller, HttpRequest, HttpResponse, Validation } from "@/presentation/protocols"
 
 export class NewVueetController implements Controller {
     private readonly newVueet: NewVueet
+    private readonly validation: Validation
     
-    constructor (newVueet: NewVueet) {
+    constructor (newVueet: NewVueet, validation: Validation) {
         this.newVueet = newVueet
+        this.validation = validation
     }
     
     async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -15,6 +17,10 @@ export class NewVueetController implements Controller {
             
             const { text, in_reply_to_user_id, source, in_reply_to_vueet_id } = httpRequest.body
             const userId = httpRequest.userId
+            
+            const error = this.validation.validate(httpRequest.body)
+            
+            if (error) return badRequest(error)
             
             const newVueet: AddVueetModel = {
                 text,
