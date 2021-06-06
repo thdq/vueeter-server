8dbb83cf415a7c211cf60cd81fb40e1b7be3433d
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-06-06 18:07:55 -0300

    refactor: add i18n message error to MissingParamsError

--- src/presentation/errors/missing-params-error.ts
+++ src/presentation/errors/missing-params-error.ts
@@ -3,6 +3,7 @@ export class MissingParamsError extends Error {
     constructor (paramName: string) {
         super(`Missing param: ${paramName}`)
         this.name = 'MissingParamsError'
+        this.message = `domain.errors.notprovided.${paramName}`
     }
 
 }
