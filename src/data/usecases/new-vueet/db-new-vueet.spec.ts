35ddab3fe31b870840c766fc6cfe13d2ad4af6b5
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-06-06 21:21:02 -0300

    test: ensure DbNewVueet calls NewVueetRepository with correct values

---
+++ src/data/usecases/new-vueet/db-new-vueet.spec.ts
@@ -0,0 +1,57 @@
+import { NewVueetRepository } from "@/data/protocols/repository/vueet/new-vueet-repository"
+import { AddVueetModel } from "@/domain/usecases/new-vueet"
+import { DbNewVueet } from "./db-new-vueet"
+import faker from 'faker'
+
+interface SutTypes {
+    sut: DbNewVueet
+    newVueetRepositoryStub: NewVueetRepository
+}
+
+const makeNewVueetRepository = (): NewVueetRepository => {
+    
+    class NewVueetRepositoryStub implements NewVueetRepository {
+        async create (fakeNewVueet): Promise<void> {
+            await null
+        }
+    }
+    
+    return new NewVueetRepositoryStub()
+    
+}
+
+const makeSut = (): SutTypes => {
+    
+    const newVueetRepositoryStub = makeNewVueetRepository()
+    const sut = new DbNewVueet(newVueetRepositoryStub)
+    
+    return {
+        sut,
+        newVueetRepositoryStub
+    }
+    
+}
+
+describe('DbNewVueet Usecase ', () => {
+
+    test('Should call NewVueetRepository with correct values', async () => {
+
+        const { sut, newVueetRepositoryStub } = makeSut()
+        
+        const newVueet: AddVueetModel = {
+            text: faker.random.word(),
+            source: faker.internet.userAgent(),
+            in_reply_to_vueet_id: null,
+            in_reply_to_user_id: null,
+            user_id: faker.datatype.uuid()
+        }
+        
+        const newVueetRepositorySpy = jest.spyOn(newVueetRepositoryStub, 'create')
+        
+        await sut.create(newVueet)
+        
+        expect(newVueetRepositorySpy).toHaveBeenCalledWith(newVueet)
+        
+    })
+
+})
