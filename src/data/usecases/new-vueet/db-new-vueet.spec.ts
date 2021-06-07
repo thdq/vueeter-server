0e55a29333646f06bd1179adc5b48e49dfb6a488
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-06-06 21:35:08 -0300

    test: ensure NewVueetRepository throws if NewVueetRepository throws

--- src/data/usecases/new-vueet/db-new-vueet.spec.ts
+++ src/data/usecases/new-vueet/db-new-vueet.spec.ts
@@ -53,5 +53,25 @@ describe('DbNewVueet Usecase ', () => {
         expect(newVueetRepositorySpy).toHaveBeenCalledWith(newVueet)
         
     })
+    
+    test('Should throw if NewVueetRepository throws', async () => {
+        
+        const { sut, newVueetRepositoryStub } = makeSut()
+        
+        jest.spyOn(newVueetRepositoryStub, 'create').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+        
+        const newVueet: AddVueetModel = {
+            text: faker.random.word(),
+            source: faker.internet.userAgent(),
+            in_reply_to_vueet_id: null,
+            in_reply_to_user_id: null,
+            user_id: faker.datatype.uuid()
+        }
+        
+        const promise = sut.create(newVueet)
+        
+        await expect(promise).rejects.toThrow()
+        
+    })    
 
 })
